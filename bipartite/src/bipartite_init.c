45aa46441e2b9fe2cde55d2247df39cb8702d765
Carsten Dormann <carsten.dormann@biom.uni-freiburg.de>
2017-03-27 09:06:38 +0200

    corrected C-function arguments in bipartite_init.c

---
+++ bipartite/src/bipartite_init.c
@@ -0,0 +1,27 @@
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* 
+generated with
+tools::package_native_routine_registration_skeleton(".")
+*/
+
+
+/* .C calls */
+extern void bmn5(int*, int*, int*, double*, int*, int*, int*,  int*,  int*,  int*, double*, double*, double*, double*, double*, double*, double*, double*, double*, int*, int*);
+extern void identifyModules(int*, char*);
+
+static const R_CMethodDef cMethods[ ] = {
+    {"bmn5", (DL_FUNC) &bmn5, 21},
+    {"identifyModules", (DL_FUNC) &identifyModules,  2 },
+    {NULL, NULL, 0}
+};
+
+/* {INTSXP, INTSXP, INTSXP, REALSXP, INTSXP, INTSXP, INTSXP,  INTSXP,  INTSXP,  INTSXP, REALSXP, REALSXP, REALSXP, REALSXP, REALSXP, REALSXP, REALSXP, REALSXP, REALSXP, INTSXP, INTSXP} */
+/* {INTSXP, STRSXP}*/
+
+void R_init_bipartite(DllInfo *dll)
+{
+    R_registerRoutines(dll, cMethods, NULL, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
