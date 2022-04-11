27bceea2f114e5c62677cb399315a0df7acbb4b9
Carsten Dormann <carsten.dormann@biom.uni-freiburg.de>
2022-04-11 10:39:23 +0200

    updated nested to yield indices in [0, 100%]

--- bipartite/R/nested.r
+++ bipartite/R/nested.r
@@ -1,14 +1,14 @@
 nested <- function(web, method="binmatnest", rescale=FALSE, normalised=TRUE){
   # a wrapper function to call any of the currently implemented measures of nestedness
  
-  if (! any(method %in% c("discrepancy", "binmatnest", "discrepancy2", "NODF", "NODF2", "weighted NODF", "wine", "C score", "checker", "ALL"))) stop("Typo? Unknown method!")
-  if ("ALL" %in% method) index <- c("binmatnest", "discrepancy", "binmatnest", "discrepancy2", "NODF", "NODF2", "weighted NODF", "wine", "C score", "checker") else index <- method
+  if (! any(method %in% c("discrepancy", "binmatnest", "discrepancy2", "NODF", "NODF2", "weighted NODF", "wine", "C score", "checker", "WNODA", "ALL"))) stop("Typo? Unknown method!")
+  if ("ALL" %in% method) index <- c("binmatnest", "discrepancy", "binmatnest", "discrepancy2", "NODF", "NODF2", "weighted NODF", "wine", "C score", "checker", "WNODA") else index <- method
 
   out <- NULL
 	if ("binmatnest" %in% index){ 
 		nessy <- try(nestedtemp(web)$statistic, silent=TRUE)
 		nessy.value <- if (inherits(nessy, "try-error")) NA else nessy
-		out <- c(out, binmatnest2 = nessy.value)
+		out <- c(out, "binmatnest" = nessy.value)
 	} 
 
   # retired:
@@ -19,12 +19,12 @@ nested <- function(web, method="binmatnest", rescale=FALSE, normalised=TRUE){
 	#}
   
   if ("discrepancy2" %in% index) {
-  	out <- c(out, "discrepancy2"=nesteddisc(web)$statistic)
+  	out <- c(out, "discrepancy2"=unname(nesteddisc(web)$statistic))
   	}
 
   if ("discrepancy" %in% index) out <- c(out, "discrepancy"=unname(discrepancy(web)))
      
-  if ("C score" %in% index) out <- c(out, "C score"=C.score(web, normalise=normalised))
+  if ("C score" %in% index) out <- c(out, "C score"=C.score(web, normalise=normalised)*100 )
   
   if ("checker" %in% index) out <- c(out, "checker"=nestedchecker(web)$C.score) 
   # identical to C.score(., FALSE)
@@ -35,9 +35,13 @@ nested <- function(web, method="binmatnest", rescale=FALSE, normalised=TRUE){
 
   if ("weighted NODF" %in% index) out <- c(out, "weighted NODF"=unname(nestednodf(web, order=TRUE, weighted=TRUE)$statistic[3]))
 	
-  if ("wine" %in% index) out <- c(out, "wine"=wine(web)$wine)
+  if ("wine" %in% index) out <- c(out, "wine"=wine(web)$wine*100 )
   
-  if ("WNODA" %in% index) out <- c(out, "WNODA"=nest.smdm(web)$WNODAmatrix)
+  if ("WNODA" %in% index){
+    mod <- computeModules(web)
+    const <- module2constraints(mod)
+    out <- c(out, "WNODA"=nest.smdm(web, constraints=const, weighted=TRUE, decreasing="abund")$WNODAmatrix)
+  } 
   
   if (rescale & ! "ALL" %in% method) warning("You requested rescaling, but you won't get it (unless you use method='ALL')!") 
   
