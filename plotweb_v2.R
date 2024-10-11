8887b445ba9655e1135fedb3b62d77182944ed0d
Tobias Bauer <tobiasbauer@online.de>
2024-10-11 09:52:29 +0200

    Fixed bugs with wrong text width for rotated text.

--- plotweb_v2.R
+++ plotweb_v2.R
@@ -1,4 +1,5 @@
 plotweb_v2 <- function(web,
+                       sorting = "none",
                        upper_abundances = NULL,
                        lower_abundances = NULL,
                        add_upper_abundances = NULL,
@@ -9,13 +10,17 @@ plotweb_v2 <- function(web,
                        srt = 0,
                        upper_italic = FALSE,
                        lower_italic = FALSE,
-                       text_size = 1,#"auto",
+                       text_size = 1,
+                       box_size = 0.1,
                        x_lim = c(0, 1),
                        y_lim = c(0, 1),
                        lower_color = "black",
                        lower_border = "same",
+                       lower_add_color = "red",
                        upper_color = "black",
                        upper_border = "same",
+                       upper_add_color = "red",
+                       text_color = "black",
                        horizontal = FALSE,
                        #abbr_names = FALSE,
                        link_color = "lower",
@@ -25,11 +30,9 @@ plotweb_v2 <- function(web,
                        arrow = "no",
                        spacing = "auto",
                        plot_axes = FALSE,
+                       add = FALSE,
                        mar = c(1, 1, 1, 1),
-                       mai = NULL)#,
-                       #legend = FALSE) {
-  #web <- sortweb2(web, sequence = NULL, empty = TRUE, sort.order = "cca")
-
+                       mai = NULL) {
   # Set the figure border via the mai or mar argument
   if (!is.null(mar) & !is.null(mai)) {
     warning("Both mar and mai have been set to values other than NULL. ",
@@ -77,6 +80,29 @@ plotweb_v2 <- function(web,
     upper_color <- upper_color[c_names]
   }
 
+  # if (upper_add_color == "same") {
+  #   upper_color <- rep(upper_color, each = 2)
+  # } else if (!is.null(names(upper_add_color))) {
+  #   stopifnot(length(upper_add_color) == nc)
+  #   if (!setequal(names(upper_add_color), c_names)) {
+  #     stop("Names of upper_add_color does not match upper species names.")
+  #   }
+  #   upper_add_color <- upper_add_color[colnames(web1)]
+  #   upper_color <- c(rbind(upper_color, upper_add_color))
+  # } else if (length(upper_add_color) < nc) {
+  #   upper_add_color <- rep_len(upper_color, nc)
+  #   upper_color <- c(rbind(upper_color, upper_add_color))
+  # }
+  
+  # if (lower_add_color == "same") {
+  #   lower_color <- rep(lower_color, each = 2)
+  # } else if (!is.null(names(lower_add_color))) {
+  #   stopifnot(length(lower_add_color) == nr)
+  #   lower_color <- c(rbind(lower_color, lower_add_color))
+  # } else {
+  #   lower_color <- c(rbind(lower_color, lower_add_color))
+  # }
+
   ## TODO: Implement this correctly and add an option to create a legend
   # if (abbr_names == "numbers") {
   #   r_names <- (nc + 1):(nc + nr)
@@ -91,6 +117,7 @@ plotweb_v2 <- function(web,
   #   r_full_names <- rownames(web)
   #   c_full_names <- colnames(web)
   # }
+
 
   if (text_size == "auto") {
     total_str_h <- sum(strheight(c_names, srt = srt))
@@ -108,39 +135,67 @@ plotweb_v2 <- function(web,
     mai[2] <- mai[2] + c_m_t_width
     mai[4] <- mai[4] + r_m_t_width
   } else {
-    mai[1] <- mai[1] + c_m_t_width
-    mai[3] <- mai[3] + r_m_t_width
+    mai[3] <- mai[3] + c_m_t_width
+    mai[1] <- mai[1] + r_m_t_width
   }
 
   par(mai = mai)
 
   if (horizontal) {
-    plot(0, type = "n",
-         xlim = x_lim,
-         ylim = c(0, 1),
-         axes = plot_axes, xlab = "", ylab = "", xaxs = "i", yaxs = "i")
+    if (add == FALSE) {
+      plot(0, type = "n",
+          xlim = x_lim,
+          ylim = y_lim,
+          axes = plot_axes, xlab = "", ylab = "", xaxs = "i", yaxs = "i")
+    }
+    space_size <- y_lim[2] - y_lim[1]
+    space_start <- y_lim[1]
   } else {
-    plot(0, type = "n",
-         ylim = y_lim,
-         xlim = c(0, 1),
-         axes = plot_axes, xlab = "", ylab = "", xaxs = "i", yaxs = "i")
+    if (add == FALSE) {
+      plot(0, type = "n",
+          ylim = y_lim,
+          xlim = x_lim,
+          axes = plot_axes, xlab = "", ylab = "", xaxs = "i", yaxs = "i")
+    }
+    space_size <- x_lim[2] - x_lim[1]
+    space_start <- x_lim[1]
   }
 
-  str_h <- 1.1 * strheight(c_names[1], srt = srt, cex = text_size)
+  print(space_size)
+
+  # if (horizontal) {
+  #   str_h <- 1.1 * strheight(c_names[1], srt = srt, cex = text_size)
+  # } else {
+  #   str_h <- 1.1 * strwidth(c_names[1], srt = srt, cex = text_size)
+  # }
+
+  theta <- srt * pi / 180
+  print(theta)
+
+  # Get the unrotated height and width of the text
+  H <- strheight(c_names[1], srt = srt, cex = text_size)
+  print(H)
+  W <- strwidth(c_names[1], srt = srt, cex = text_size)
+  print(W)
+
+  # Calculate the effective rotated height and width
+  str_h <- abs(H * cos(theta)) + abs(W * sin(theta))
+
+  print(str_h)
 
   if (length(spacing) == 2) {
-    c_space <- spacing[1] / (nc - 1)
-    r_space <- spacing[2] / (nr - 1)
+    c_space <- space_size * spacing[1] / (nc - 1)
+    r_space <- space_size * spacing[2] / (nr - 1)
   } else if (length(spacing) == 1) {
     if (is.numeric(spacing)) {
       spacing <- c(spacing, spacing)
-      c_space <- spacing[1] / (nc - 1)
-      r_space <- spacing[2] / (nr - 1)
-    }else if (spacing == "auto")  {
-      space <- max(nc, nr) * str_h
+      c_space <- space_size * spacing[1] / (nc - 1)
+      r_space <- space_size * spacing[2] / (nr - 1)
+    } else if (spacing == "auto")  {
+      space <- space_size * max(nc, nr) * str_h
       if (space > 1) {
-        warning(paste("Text size is too large for auto spacing.",
-                      "Decrease size or increase figure height for better results."))
+        stop(paste("Text size is too large for auto spacing.",
+                   "Decrease size or increase figure height for better results."))
       }
       spacing <- c(space, space)
       r_space <- space / (nr - 1)
@@ -176,14 +231,14 @@ plotweb_v2 <- function(web,
   }
 
   if (scaling == "relative") {
-    c_prop_sizes <- (1 - spacing[1]) * upper_abundances / sum(upper_abundances)
-    r_prop_sizes <- (1 - spacing[2]) * lower_abundances / sum(lower_abundances)
+    c_prop_sizes <- (space_size - spacing[1]) * upper_abundances / sum(upper_abundances)
+    r_prop_sizes <- (space_size - spacing[2]) * lower_abundances / sum(lower_abundances)
   } else if (scaling == "absolute") {
     max_abundances <- max(sum(upper_abundances), sum(lower_abundances))
-    c_prop_sizes <- (1 - spacing[1]) * upper_abundances / max_abundances
-    r_prop_sizes <- (1 - spacing[2]) * lower_abundances / max_abundances
-    c_space <- (1 - sum(c_prop_sizes)) / (nc - 1)
-    r_space <- (1 - sum(r_prop_sizes)) / (nr - 1)
+    c_prop_sizes <- (space_size - spacing[1]) * upper_abundances / max_abundances
+    r_prop_sizes <- (space_size - spacing[2]) * lower_abundances / max_abundances
+    c_space <- (space_size - sum(c_prop_sizes)) / (nc - 1)
+    r_space <- (space_size - sum(r_prop_sizes)) / (nr - 1)
   }
 
   if (!is.null(add_upper_abundances)) {
@@ -195,11 +250,11 @@ plotweb_v2 <- function(web,
     nr <- nr * 2
   }
 
-  c_xl <- c(0, cumsum(c_prop_sizes[-nc] + c_space))
+  c_xl <- c(space_start, cumsum(c_prop_sizes[-nc] + c_space))
   c_xr <- c(c_prop_sizes[1],
             c_prop_sizes[1] + cumsum(c_prop_sizes[-1] + c_space))
 
-  r_xl <- c(0, cumsum(r_prop_sizes[-nr] + r_space))
+  r_xl <- c(space_start, cumsum(r_prop_sizes[-nr] + r_space))
   r_xr <- c(r_prop_sizes[1],
             r_prop_sizes[1] + cumsum(r_prop_sizes[-1] + r_space))
 
@@ -233,24 +288,38 @@ plotweb_v2 <- function(web,
     r_names <- as.expression(r_names)
   }
 
+  if (length(box_size) == 1) {
+    upper_box_size <- box_size
+    lower_box_size <- box_size
+  } else if (length(box_size) == 2) {
+    upper_box_size <- box_size[1]
+    lower_box_size <- box_size[2]
+  }
+
   # Draw the boxes and labels either horizontal or vertical to each other.
   if (horizontal) {
-    rect(0, c_xl, 0.1, c_xr, col = upper_color, border = upper_border)
-    rect(0.9, r_xl, 1, r_xr, col = lower_color, border = lower_border)
-    text(-0.01, c_tx, c_names, adj = c(1, 0.5), srt = srt, cex = text_size,
-         xpd = TRUE, font = font, family = family)
-    text(1.01, r_tx, r_names, adj = c(0, 0.5), srt = srt, cex = text_size,
-         xpd = TRUE, font = font, family = family)
+    x_start <- x_lim[1]
+    x_end <- x_lim[2]
+    rect(x_start, c_xl, x_start + upper_box_size, c_xr,
+         col = upper_color, border = upper_border)
+    rect(x_end - lower_box_size, r_xl, x_end, r_xr,
+         col = lower_color, border = lower_border)
+    text(x_start - 0.01, c_tx, c_names, adj = c(1, 0.5), srt = srt,
+         cex = text_size, xpd = TRUE, font = font, family = family)
+    text(x_end + 0.01, r_tx, r_names, adj = c(0, 0.5), srt = srt,
+         cex = text_size, xpd = TRUE, font = font, family = family)
   } else {
-    rect(r_xl, 0, r_xr, 0.1, col = lower_color, border = lower_border)
-    rect(c_xl, 0.9, c_xr, 1, col = upper_color, border = upper_border)
-    text(r_tx, -0.01, r_names, adj = c(1, 0.5), cex = text_size,
-         xpd = TRUE, srt = 90 + srt, font = font, family = family)
+    y_start <- y_lim[1]
+    y_end <- y_lim[2]
+    rect(r_xl, y_start, r_xr, y_start + lower_box_size, col = lower_color, border = lower_border)
+    rect(c_xl, y_end - upper_box_size, c_xr, y_end, col = upper_color, border = upper_border)
+    text(r_tx, y_start - 0.01, r_names, adj = c(1, 0.5), cex = text_size,
+         xpd = TRUE, srt = srt + 90, font = font, family = family)
     # text(r_tx, 1.0 + strwidth(r_names, srt = srt) / 2, r_names,
     #      adj = c(.5, 0.5), cex = text_size, xpd = TRUE, srt = 90 + srt,
     #      font = font, family = family)
-    text(c_tx, 1.01, c_names,
-         adj = c(0, 0.5), cex = text_size, xpd = TRUE, srt = 90 + srt,
+    text(c_tx, y_end + 0.01, c_names,
+         adj = c(0, 0.5), cex = text_size, xpd = TRUE, srt = srt + 90,
          font = font, family = family)
   }
 
@@ -284,7 +353,7 @@ plotweb_v2 <- function(web,
     relpos <- cumsum(links.i$weight) / sum(links.i$weight)
     coords.int.low <- (r_xl[i] + relpos * (r_xr[i] - r_xl[i]))
     web.df[web.df$row == i, "xcoord.bl"] <- c(r_xl[i], coords.int.low[-nrow(links.i)])
-    web.df[web.df$row == i, "xcoord.br"] <- c(coords.int.low)    
+    web.df[web.df$row == i, "xcoord.br"] <- c(coords.int.low)
     if (arrow %in% c("down.center", "both.center")) {
       web.df[web.df$row == i, "xcoord.bl"] <- web.df[web.df$row == i, "xcoord.br"] <- mean(c(r_xl[i], r_xr[i]))
     }
@@ -299,7 +368,7 @@ plotweb_v2 <- function(web,
     relpos <- cumsum(links.j$weight) / sum(links.j$weight)
     coords.int.high <- (c_xl[j] + relpos * (c_xr[j] - c_xl[j]))
     web.df[web.df$col == j, "xcoord.tl"] <- c(c_xl[j], coords.int.high[-nrow(links.j)])
-    web.df[web.df$col == j, "xcoord.tr"] <- c(coords.int.high)    
+    web.df[web.df$col == j, "xcoord.tr"] <- c(coords.int.high)
     if (arrow %in% c("up.center", "both.center")) {
       web.df[web.df$col == j, "xcoord.tl"] <- web.df[web.df$col == j, "xcoord.tr"] <- mean(c(c_xl[j], c_xr[j]))
     }
@@ -310,11 +379,11 @@ plotweb_v2 <- function(web,
   for (linki in order(-web.df$weight)) {
     link <- web.df[linki, ]
     if (horizontal) {
-      x1 <- 0.1
-      x2 <- 0.9
+      x1 <- x_start + upper_box_size
+      x2 <- x_end - lower_box_size
     } else {
-      x1 <- 0.9
-      x2 <- 0.1
+      x1 <- y_end - lower_box_size#0.9
+      x2 <- y_start + upper_box_size#0.1
     }
     y1 <- link$xcoord.tl
     y2 <- link$xcoord.bl
@@ -333,6 +402,7 @@ plotweb_v2 <- function(web,
               style = style)
   }
 
+  ## TODO: Implement legend plotting
   # if (legend == TRUE) {
   #   plot()
   # }
