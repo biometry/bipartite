7e7f7d858c5f567ba91565c8e2a1d33d803f1448
Tobias Bauer <t.bauer@fz-juelich.de>
2024-10-21 13:12:04 +0200

    Added support for custom additional abundance color in plotweb_v2.R

--- plotweb_v2.R
+++ plotweb_v2.R
@@ -80,29 +80,6 @@ plotweb_v2 <- function(web,
     upper_color <- upper_color[c_names]
   }
 
-  # if (upper_add_color == "same") {
-  #   upper_color <- rep(upper_color, each = 2)
-  # } else if (!is.null(names(upper_add_color))) {
-  #   stopifnot(length(upper_add_color) == nc)
-  #   if (!setequal(names(upper_add_color), c_names)) {
-  #     stop("Names of upper_add_color does not match upper species names.")
-  #   }
-  #   upper_add_color <- upper_add_color[colnames(web1)]
-  #   upper_color <- c(rbind(upper_color, upper_add_color))
-  # } else if (length(upper_add_color) < nc) {
-  #   upper_add_color <- rep_len(upper_color, nc)
-  #   upper_color <- c(rbind(upper_color, upper_add_color))
-  # }
-  
-  # if (lower_add_color == "same") {
-  #   lower_color <- rep(lower_color, each = 2)
-  # } else if (!is.null(names(lower_add_color))) {
-  #   stopifnot(length(lower_add_color) == nr)
-  #   lower_color <- c(rbind(lower_color, lower_add_color))
-  # } else {
-  #   lower_color <- c(rbind(lower_color, lower_add_color))
-  # }
-
   ## TODO: Implement this correctly and add an option to create a legend
   # if (abbr_names == "numbers") {
   #   r_names <- (nc + 1):(nc + nr)
@@ -161,6 +138,7 @@ plotweb_v2 <- function(web,
     space_start <- x_lim[1]
   }
 
+  print("-------------------------------------------")
   print(space_size)
 
   # if (horizontal) {
@@ -213,8 +191,24 @@ plotweb_v2 <- function(web,
     # Sort the additional upper abundances according to the column-order in the web
     add_upper_abundances <- add_upper_abundances[colnames(web)]
     upper_abundances <- c(rbind(upper_abundances, add_upper_abundances))
-    #c_space <- c(0, c_space)
-    upper_color <- rep(upper_color, each = 2)
+    # Include the custom colors into the color vector
+    if (length(upper_add_color) == 1) {
+      if (upper_add_color == "same") {
+        upper_add_color <- upper_color
+        # upper_color <- rep(upper_color, each = 2)
+      } else {
+        upper_add_color <- rep_len(upper_add_color, nc)
+      }
+    } else if (!is.null(names(upper_add_color))) {
+      stopifnot(length(upper_add_color) == nc)
+      if (!setequal(names(upper_add_color), c_names)) {
+        stop("Names of upper_add_color does not match upper species names.")
+      }
+      upper_add_color <- upper_add_color[colnames(web)]
+    } else if (length(upper_add_color) < nc) {
+      upper_add_color <- rep_len(upper_add_color, nc)
+    }
+    upper_color <- c(rbind(upper_color, upper_add_color))
   }
 
   if (is.null(lower_abundances)) {
@@ -227,7 +221,24 @@ plotweb_v2 <- function(web,
     # by alternating indices
     lower_abundances <- c(rbind(lower_abundances, add_lower_abundances))
     #r_space <- c(0, r_space)
-    lower_color <- rep(lower_color, each = 2)
+    # Include the custom colors into the color vector
+    if (length(lower_add_color) == 1) {
+      if (lower_add_color == "same") {
+        lower_add_color <- lower_color
+        # upper_color <- rep(upper_color, each = 2)
+      } else {
+        lower_add_color <- rep_len(lower_add_color, nr)
+      }
+    } else if (!is.null(names(lower_add_color))) {
+      stopifnot(length(lower_add_color) == nr)
+      if (!setequal(names(lower_add_color), r_names)) {
+        stop("Names of lower_add_color does not match lower species names.")
+      }
+      lower_add_color <- lower_add_color[rownames(web)]
+    } else if (length(lower_add_color) < nr) {
+      lower_add_color <- rep_len(lower_add_color, nr)
+    }
+    lower_color <- c(rbind(lower_color, lower_add_color))
   }
 
   if (scaling == "relative") {
@@ -242,12 +253,12 @@ plotweb_v2 <- function(web,
   }
 
   if (!is.null(add_upper_abundances)) {
-    c_space <- c(0, c_space)
     nc <- nc * 2
+    c_space <- rep(c(0, c_space), length.out = (nc - 1))
   }
   if (!is.null(add_lower_abundances)) {
-    r_space <- c(0, r_space)
     nr <- nr * 2
+    r_space <- rep(c(0, r_space), length.out = (nr - 1))
   }
 
   c_xl <- c(space_start, cumsum(c_prop_sizes[-nc] + c_space))
