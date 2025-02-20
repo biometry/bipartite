48f1b555b050351faa54a5eabdb5ede5c376ca6d
Tobias Bauer <tobiasbauer@online.de>
2025-02-20 16:11:40 +0100

    Frame2webs optim (#25)
    * Added fast implementation of frame2webs in function frame2webs_fast
    
    * Added frame2webs_test.R with tests and benchmarks of ne frame2webs implementation
    
    * Added comments to new frame2webs function + changed benchmark function in frame2webs_tests.R
    
    * Fixed a bug in frame2webs occuring when type.out == list and emptylist = TRUE and the data has only one upper or lower species
    
    * removed frame2webs_old

--- bipartite/R/frame2webs.R
+++ bipartite/R/frame2webs.R
@@ -1,16 +1,84 @@
-frame2webs <- function(dframe, varnames = c("lower", "higher", "webID", "freq"), type.out = "list", emptylist = TRUE) {
-  # author: Jochen Fruend
-  if (length(varnames)==4) {
-    if (any(is.na(dframe[,varnames[4]]))) warning(paste("NAs in", varnames[4], "converted to 0"))
-    webarray <- tapply(dframe[,varnames[4]],dframe[,varnames[1:3]], sum)
+###
+# @author: Tobias Bauer, Jochen Fruend
+# Convenience function to convert a table of observations
+# (i.e. an "edge list", typically compiled in a spreadsheet programm)
+# into a network matrix for further use in bipartite.
+#
+# Optimization with partial reimplementation of tapply function by Tobias Bauer
+frame2webs <- function(dframe,
+                       varnames = c("lower", "higher", "webID", "freq"),
+                       type.out = "list", emptylist = TRUE) {
+  # If only 3 varnames are defined create a vector filled with 1s
+  if (length(varnames) == 4) {
+    values <- dframe[, varnames[4]]
+  } else if (length(varnames) == 3) {
+    values <- rep(1, nrow(dframe))
   }
-  if (length(varnames)==3) webarray <- tapply(rep(1,nrow(dframe)),dframe[,varnames[1:3]], sum)
-  webarray[is.na(webarray)] <- 0   # needs to be done when using tapply: unobserved combinations always get a zero, even with na.rm=T
-  if (type.out=="array") return(webarray)
-  if (type.out=="list") {
+  # !!! THE FOLLOWING CODE IS ALMOST AN EXACT COPY OF
+  # THE STANDARD R FUNCTION TAPPLY WITH MINOR
+  # CHANGES FOR OPTIMIZATION !!!
+  # Convert all three the indices to factors
+  INDEX <- lapply(dframe[, varnames[1:3]], as.factor)
+  # Number of indices
+  nI <- 3
+  # Get the levels of all three indices
+  namelist <- lapply(INDEX, levels)
+  # Get the number of levels for all three indices
+  extent <- lengths(namelist, use.names = FALSE)
+  # Calculate the cumulative product of the index lengths
+  # i.e n(index 1) * n(index 2) * n(index 3)
+  # This value is equal to the total size of the output array.
+  cumextent <- cumprod(extent)
+  # Throw an error if the total size is larger than the maximum integer
+  # size of the machine.
+  if (cumextent[nI] > .Machine$integer.max)
+    stop("total number of levels >= 2^31")
+  storage.mode(cumextent) <- "integer"
+  # > ngroup <- cumextent[nI]
+  # Convert the index combination to unique integers
+  group <- as.integer(INDEX[[1L]])
+  if (nI > 1L) {
+    for (i in 2L:nI) {
+      group <- group + cumextent[i - 1L] * (as.integer(INDEX[[i]]) - 1L)
+    }
+  }
+  # In the standard tapply function at this point a factor of all integers
+  # up to ngroup is generated. In my opinion this is unnecessary and consumes
+  # way to much memory.
+  # > levels(group) <- as.character(seq_len(ngroup))
+  # > class(group) <- "factor"
+  # > print(group)
+  # > print(namelist)
+  # Split the observation data only regarding index combination,
+  # that are actually present in the data.frame (drop = TRUE).
+  # That way the calculation time for sparse matrices is reduced immensely.
+  ans <- split(values, group, drop = TRUE)
+  # Now apply the sum function on the sparse split list.
+  ans <- lapply(ans, sum)
+  # Create the resulting array with the full extent of all possible
+  # index combination. Non-existing combinations are filled
+  # with the default value 0. The dimension names are taken from the
+  # factor levels of the indices.
+  ansmat <- array(0, dim = extent, dimnames = namelist)
+  # Now fill the array at the location for which index combination
+  # existed in the data.frame with the calculated sum-values.
+  ansmat[as.integer(names(ans))] <- unlist(ans, recursive = FALSE,
+                                           use.names = FALSE)
+  if (type.out == "array")
+    return(ansmat)
+  # Convert array to list and throw away empty rows or columns if emptylist == T
+  if (type.out == "list") {
     weblist <- list()
-    for (i in dimnames(webarray)[[3]]) weblist[[i]] <- webarray[,,i]
-    if (emptylist) weblist <- lapply(weblist,empty)
+    for (i in dimnames(ansmat)[[3]]) {
+      # Because of the nonsensical behavior in R, that either all dimensions
+      # of length 1 or no dimensions at all are dropped we need this weird
+      # workaround...
+      weblist[[i]] <- array(ansmat[, , i, drop = FALSE],
+                            dim = extent[-3],
+                            dimnames = namelist[-3])
+    }
+    if (emptylist)
+      weblist <- lapply(weblist, empty)
     return(weblist)
   }
 }
