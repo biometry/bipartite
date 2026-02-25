ff8b501c34471234698e5ffbb0c94fb53bacaf59
Tobias Bauer <tobias.bauer-2@uni-hamburg.de>
2026-02-25 16:53:00 +0100

    making MersenneTwister Code compatible with C++20

--- bipartite/src/MersenneTwister.h
+++ bipartite/src/MersenneTwister.h
@@ -71,11 +71,11 @@ class MTRand {
 public:
 	typedef unsigned long uint32;  // unsigned integer type, at least 32 bits
 	
-	enum { N = 624 };       // length of state vector
+	static const int N = 624;       // length of state vector
 	enum { SAVE = N + 1 };  // length of array for save()
 
 protected:
-	enum { M = 397 };  // period parameter
+	static const int M = 397;  // period parameter
 	
 	uint32 state[N];   // internal state
 	uint32 *pNext;     // next value to get from state
